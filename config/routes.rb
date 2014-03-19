fb7fbc69aedab8063906490173fedd451f5f7bbd
Tyler Shields <shieldstyler@rocketmail.com>
2014-03-18 22:53:34 -0700

    Sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Victorchannel::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'get'
   match '/legal', to: 'static_pages#legal', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
