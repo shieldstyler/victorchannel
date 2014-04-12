9f9fd56487a31f839fc097cf020a6c770ccdb677
Tyler Shields <shieldstyler@rocketmail.com>
2014-04-12 02:10:24 -0700

    Add Basic Styling

--- db/migrate/20140401022125_create_articles.rb
+++ db/migrate/20140401022125_create_articles.rb
@@ -3,11 +3,12 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.integer :author_id
       t.string :title
+      t.string :shortname
       t.datetime :published
       t.datetime :edited
 
       t.timestamps
     end
-    add_index :articles, [:author_id, :created_at]
+    add_index :articles, [:author_id, :created_at, :slug]
   end
 end
